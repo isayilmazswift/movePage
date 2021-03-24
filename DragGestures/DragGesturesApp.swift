cfa5962e110825de3369df5c27696b57167f4fc7
isa yilmaz <jesus.codes@icloud.com>
2021-03-24 16:31:00 +0300

    Initial Commit

---
+++ DragGestures/DragGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragGesturesApp.swift
+//  DragGestures
+//
+//  Created by İsa Yılmaz on 24.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct DragGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
